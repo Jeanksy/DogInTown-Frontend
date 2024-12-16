9cb4faded6a9f7ed4cf7a55664d5bc005febe063
Gabriel <gabrielbenoit87@gmail.com>
2024-12-16 09:15:27 +0100

    app.js HomeScreen back

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ export default function App() {
       <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Home" component={DogsInfoScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="SignIn" component={SignInScreen} />
             <Stack.Screen name="SignUp" component={SignUpScreen} /> 
             <Stack.Screen name="DogSignUp" component={DogSignUpScreen} />
