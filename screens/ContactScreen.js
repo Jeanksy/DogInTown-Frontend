85acecc48adbceb360bf82995d5df8348ce94b9e
Amandine <amandinejprat@gmail.com>
2024-12-18 17:26:54 +0100

    sauvegarde fin de journee mercredi

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Pressable } from 'react-native';
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 
-export default function ContactScreen() {
-  
+export default function ContactScreen({navigation}) {
+  const handleReturn = () => {
+		navigation.navigate('Options');
+	}
+
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.background}>
             <Pressable style={styles.fleche} onPress={() => handleReturn()}><FontAwesome name="arrow-left" size={30} color="#A23D42" textAlign='right'></FontAwesome></Pressable>
-       <Text>Contactez-nous par mail sur dogintown@contact.fr</Text>
-       <Image/>
+       <FontAwesome name='envelope' color='white' size={200}/>
+       <Text style={styles.texte}>Contactez-nous par mail sur dogintown@contact.fr</Text>
        </SafeAreaView>
       </View>
     );
@@ -27,9 +30,15 @@ export default function ContactScreen() {
       width: "100%",
       height: "100%",
       backgroundColor: "#F7CC99",
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     fleche: {
       marginTop: '5%',
       marginLeft: '5%',
+      alignSelf: 'Left',
     },
+    texte: {
+      fontSize: 30,
+    }
   });
